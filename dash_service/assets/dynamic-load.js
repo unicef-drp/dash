58a00b40e9045e36c8220ba6dfcad3773c109a39
Amy Reidy <73396449+Amy-Reidy@users.noreply.github.com>
2026-07-12 14:11:36 +0200

    Update dynamic-load.js

--- dash_service/assets/dynamic-load.js
+++ dash_service/assets/dynamic-load.js
@@ -217,8 +217,8 @@ if (browserOk) {
                         }
                         if (script.id == '_dash-config') {
                             config = JSON.parse(script.innerHTML);
-                            config["url_base_pathname"] = baseUrl.href;
-                            config["requests_pathname_prefix"] = baseUrl.pathname;
+                            config["url_base_pathname"] = baseUrl;
+                            config["requests_pathname_prefix"] = "";
                             script.innerHTML = JSON.stringify(config);
                         }
                         scriptElement.innerHTML = script.innerHTML;
